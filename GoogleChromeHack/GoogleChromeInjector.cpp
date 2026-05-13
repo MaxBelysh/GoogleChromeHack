5cbda63974d4f24b6d3df87385e4e10b6ade6c78
MaxBelysh <max.maxmin.maxtag@gmail.com>
2026-05-13 21:58:17 +0300

    eldenring change on chrome

--- GoogleChromeHack/GoogleChromeInjector.cpp
+++ GoogleChromeHack/GoogleChromeInjector.cpp
@@ -13,25 +13,25 @@ int main() {
 		return -1;
 	}
 
-	std::cout << "INFO: processID of eldenring.exe = " << processID << std::endl;
+	std::cout << "INFO: processID of Chrome.exe = " << processID << std::endl;
 
-	HANDLE EldenRingProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, processID);
+	HANDLE ChromeProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, processID);
 
-	if (EldenRingProcess == INVALID_HANDLE_VALUE) {
+	if (ChromeProcess == INVALID_HANDLE_VALUE) {
 		std::cout << "ERROR: failed to open process" << std::endl;
 		return -1;
 	}
 
-	std::string path = "C:\\Users\\maxma\\source\\repos\\MaxBelysh\\EldenRingCHEATS\\x64\\Debug\\EldenRingCHEATS_DLL.dll";
+	std::string path = "C:\\Users\\maxma\\source\\repos\\MaxBelysh\\GoogleChromeHack\\x64\\Debug\\DLL.dll";
 
-	LPVOID PathAddress = VirtualAllocEx(EldenRingProcess, NULL, strlen(path.c_str()) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	LPVOID PathAddress = VirtualAllocEx(ChromeProcess, NULL, strlen(path.c_str()) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
 	if (PathAddress == nullptr) {
 		std::cout << "ERROR: failed to allocate memory for dll path" << std::endl;
 		return -1;
 	}
 
-	if (!WriteProcessMemory(EldenRingProcess, PathAddress, path.c_str(), strlen(path.c_str()) + 1, NULL)) {
+	if (!WriteProcessMemory(ChromeProcess, PathAddress, path.c_str(), strlen(path.c_str()) + 1, NULL)) {
 		std::cout << "ERROR: failed to write process memory" << std::endl;
 		return -1;
 	}
@@ -49,18 +49,17 @@ int main() {
 		std::cout << "ERROR: failed to get LoadLibraryA address" << std::endl;
 		return -1;
 	}
-	Sleep(5000);
 
-	HANDLE EldenRingRemoteThread = CreateRemoteThread(EldenRingProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA_Address,
+	HANDLE ChromeRemoteThread = CreateRemoteThread(ChromeProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA_Address,
 		PathAddress, 0, NULL);
 
-	if (EldenRingRemoteThread == INVALID_HANDLE_VALUE || EldenRingRemoteThread == 0) {
+	if (ChromeRemoteThread == INVALID_HANDLE_VALUE || ChromeRemoteThread == 0) {
 		std::cout << "ERROR: failed to create remote thread" << std::endl;
 		return -1;
 	}
 
-	CloseHandle(EldenRingProcess);
-	//CloseHandle(EldenRingRemoteThread);
+	CloseHandle(ChromeProcess);
+	//CloseHandle(ChromeRemoteThread);
 
 	std::cout << "INFO: DLL injected successfully" << std::endl;
 	return 0;
